be426890d4cdf372d0b5cee72a70608c8da03beb
AJ ONeal <coolaj86@gmail.com>
2021-10-01 02:32:54 -0600

    WIP: simplify somehow? ...

--- debounce.js
+++ debounce.js
@@ -12,16 +12,18 @@ var Debouncer;
   }
 
   Debouncer.create = function _debounce(fn, delay) {
-    let state = { running: false, _timer: null, _promise: null };
+    let running = false;
+    let timer = null;
+    let promise = null;
 
     function rePromise() {
       // all three of these should be set synchronously
-      state._promise = {};
-      state._promise.promise = new Promise(function (resolve, reject) {
-        state._promise.resolve = resolve;
-        state._promise.reject = reject;
+      promise = {};
+      promise.promise = new Promise(function (resolve, reject) {
+        promise.resolve = resolve;
+        promise.reject = reject;
       });
-      return state._promise;
+      return promise;
     }
 
     rePromise();
@@ -29,29 +31,29 @@ var Debouncer;
     let err = new Error("debounce");
     return async function _debounce() {
       let args = Array.prototype.slice.call(arguments);
-      if (state.running) {
+      if (running) {
         throw err;
       }
 
-      let pInfo = state._promise;
-      if (state._timer) {
-        clearTimeout(state._timer);
+      let pInfo = promise;
+      if (timer) {
+        clearTimeout(timer);
         pInfo.reject(err);
         pInfo = rePromise();
       }
 
-      state._timer = setTimeout(function () {
-        state.running = true;
-        state._timer = null;
+      timer = setTimeout(function () {
+        running = true;
+        timer = null;
         rePromise();
 
         fn.apply(null, args)
           .then(function (result) {
-            state.running = false;
+            running = false;
             pInfo.resolve(result);
           })
           .catch(function (err) {
-            state.running = false;
+            running = false;
             pInfo.reject(err);
           });
       }, delay);
